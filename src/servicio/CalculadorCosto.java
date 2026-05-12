fb5dc27e39f82fcb4a726caba948e98877864da8
LautaroAcosta1 <acostalauty765@gmail.com>
2026-05-12 04:30:05 -0300

    Se cambia la formula de distancia. Ya que con el sistema anterior no se calculaban los kilometros reales. Para esto se usa la formula Haversine.

--- src/servicio/CalculadorCosto.java
+++ src/servicio/CalculadorCosto.java
@@ -36,9 +36,30 @@ public class CalculadorCosto {
 
     private double calcularDistancia(Localidad a, Localidad b) {
 
-        double dx = a.getLatitud() - b.getLatitud();
-        double dy = a.getLongitud() - b.getLongitud();
+        double lat1 = Math.toRadians(a.getLatitud());
+        double lon1 = Math.toRadians(a.getLongitud());
 
-        return Math.sqrt(dx * dx + dy * dy);
+        double lat2 = Math.toRadians(b.getLatitud());
+        double lon2 = Math.toRadians(b.getLongitud());
+
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double haversine =
+                Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                        + Math.cos(lat1)
+                        * Math.cos(lat2)
+                        * Math.sin(dLon / 2)
+                        * Math.sin(dLon / 2);
+
+        double c =
+                2 * Math.atan2(
+                        Math.sqrt(haversine),
+                        Math.sqrt(1 - haversine)
+                );
+
+        double radioTierra = 6371;
+
+        return radioTierra * c;
     }
 }
